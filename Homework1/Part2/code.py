9c2acf584c90d3efaf2f51006353f828155fc33e
RicMat <da.ri.mat42@gmail.com>
2020-04-04 20:28:30 +0200

    Update code.py

--- Homework1/Part2/code.py
+++ Homework1/Part2/code.py
@@ -1,67 +1,171 @@
-import os
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 12:43:22 2020
+
+@author: darim
+"""
+
 import numpy as np
 import cv2
+import random
+
+def film_response_recovery(Z, B, l, w):
+    # i is number of samples
+    i = 0
+    return i
+    
+# 2.1 and Abstract A
+#
+# Z(i,j) pixel value of pixel i in image j - i is number of samples - j is number of images
+# B(j) is log delta t for image j
+# l is lambda
+# w(z) is the weighting function - defined outside
+    
+def gsolve(images, exposures): 
+    Z = sample_images(images)
+    B = exposures
+    l = 100.
+    
+    Z_min = 0
+    Z_max = 255
+    
+    n = 255
+    
+    A = np.zeros((Z.shape[0] * Z.shape[1] + n , n + Z.shape[0]), dtype=np.float64)
+    b = np.zeros((A.shape[0], 1))
+    
+    # data fitting equation
+    
+    k = 0
+    for i in range(Z.shape[0]):
+        for j in range(Z.shape[1]):
+            zijp1 = Z[i, j] + 1
+            wij = weighting_function(zijp1)
+            A[k, zijp1] = wij # only integers as index
+            A[k, n+i] = -wij # n+i+1
+            b[k,0] = wij * B[j] # in the pdf is [i, j] but is defined as [j] only
+            k += 1
+    
+
+    #fix curve - set middle value to 0
+    
+    A[k, (Z_max - Z_min) // 2] = 1 # optional - since Z_min and Z_max are fixed, just place the mid value
+    
+    
+    #smoothness equation
+    
+    for i in range(Z_min, Z_max):  # optional - since Z_min and Z_max are fixed, just place the two values
+        w_ip1 = weighting_function(i+1)
+        A[k,i] = l * w_ip1
+        A[k,i+1] = -2 * l * w_ip1
+        A[k,i+2] = l * w_ip1
+        
+    #svd
+        
+    A_inv = np.linalg.pinv(A)
+    x = np.dot(A_inv, b)
+    g = x[0: n+1]
+    #lE = x[n+1: x.shape[0]]
+            
+    return g[:, 0]
+    
+def sample_images(images):
+    Z_min = 0
+    Z_max = 255
+    n = 256
+    
+    num_images = len(images)
+    intensity = np.zeros((n, num_images), dtype=np.uint8)
+
+    mid_img = images[num_images // 2] # optional - since Z_min and Z_max are fixed, just place the mid value
+
+    for i in range(Z_min, Z_max + 1): # optional - since Z_min and Z_max are fixed, just place the two values
+        rows, cols= np.where(mid_img == i)
+        if len(rows) != 0:
+            idx = random.randrange(len(rows))
+            for j in range(num_images):
+                intensity[i, j] = images[j][rows[idx], cols[idx]]
+    return intensity
+    
+def weighting_function(z):
+    Z_min = 0.
+    Z_max = 255.
+    
+    if z <= (Z_min + Z_max) / 2.:
+        return z - Z_min
+    else:
+        return Z_max - z
+    
+def merger(images, exposures, response): #2.2
+    img_shape = images[0].shape
+    img_rad_map = np.zeros(img_shape, dtype=np.float64)
+    
+    num_images = len(images)
+    
+    for i in range(img_shape[0]):
+        for j in range(img_shape[1]):
+            g = np.array([response[images[k][i, j]] for k in range(num_images)])
+            w = np.array([weighting_function(images[k][i, j]) for k in range(num_images)])
+            SumW = np.sum(w)
+            if SumW > 0:
+                img_rad_map[i, j] = np.sum(w * (g - exposures) / SumW)
+            else:
+                img_rad_map[i, j] = g[num_images // 2] - exposures[num_images // 2]
+    return img_rad_map
 
 
-folder = 'Memorial'
-
-# We get all the image files from the source folder
-files = list([os.path.join(folder, f) for f in os.listdir(folder)])
-
-# We compute the average by adding up the images
-# Start from an explicitly set as floating point, in order to force the
-# conversion of the 8-bit values from the images, which would otherwise overflow
-average = cv2.imread(files[0]).astype(np.float)
-for file in files[1:]:
-    image = cv2.imread(file)
-    # NumPy adds two images element wise, so pixel by pixel / channel by channel
-    average += image
- 
-# Divide by count (again each pixel/channel is divided)
-average /= len(files)
-
-# Normalize the image, to spread the pixel intensities across 0..255
-# This will brighten the image without losing information
-output = cv2.normalize(average, None, 0, 255, cv2.NORM_MINMAX)
-
-# Save the output
-cv2.imwrite('output.png', output)
-
-#hdr
-
-def loadExposureSeq(path):
-    images = []
-    times = []
-    with open('list.txt') as f:
-        content = f.readlines()
-    for line in content:
-        tokens = line.split()
-        images.append(cv2.imread(os.path.join(path, tokens[0])))
-        times.append(1. / float(tokens[1]))
-    return images, np.asarray(times, dtype=np.float32)
+def globalToneMapping(image, gamma):
+    
+    image_corrected = cv2.pow(image/255., 1.0/gamma)
+    return image_corrected
 
 
-images, exposures = loadExposureSeq('images')
+def intensityAdjustment(image, template):
+    
+    m, n, channel = image.shape
+    output = np.zeros((m, n, channel))
+    for ch in range(channel):
+        image_avg, template_avg = np.average(image[:, :, ch]), np.average(template[:, :, ch])
+        output[..., ch] = image[..., ch] * (template_avg / image_avg)
+
+    return output
+
+# Read all the files with OpenCV
+files = ['images/memorial00.png', 'images/memorial01.png', 'images/memorial02.png', 'images/memorial03.png',\
+         'images/memorial04.png', 'images/memorial05.png', 'images/memorial06.png', 'images/memorial07.png',\
+         'images/memorial08.png', 'images/memorial09.png', 'images/memorial10.png', 'images/memorial11.png',\
+         'images/memorial12.png', 'images/memorial13.png', 'images/memorial14.png', 'images/memorial15.png']
+images = list([cv2.imread(f) for f in files])
+# Compute the exposure times in seconds
+exposures = np.float32([1. / t for t in [0.03125, 0.0625, 0.125, 0.25, 0.5, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]])
 
 # Compute the response curve
-calibration = cv2.createCalibrateDebevec()
-response = calibration.process(images, exposures)
 
-# Compute the HDR image
-merge = cv2.createMergeDebevec()
-hdr = merge.process(images, exposures, response)
+num_channels = images[0].shape[2]
+hdr_image = np.zeros(images[0].shape, dtype=np.float64)
 
-# Save it to disk
-cv2.imwrite('hdr_image.hdr', hdr)
+for channel in range(num_channels):  
+    print("start {}     ".format(channel))
+    layer_stack = [img[:, :, channel] for img in images]
+    response = gsolve(layer_stack, exposures)
+    print("mid1 {}     ".format(channel))
+    # Compute the HDR image
 
+    hdr = merger(layer_stack, exposures, response)
+    print("mid2 {}     ".format(channel))
+    hdr_image[..., channel] = cv2.normalize(hdr, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    print("end {}     ".format(channel))
+    
+    
 
-tonemap = cv2.createTonemap(2.2)
-ldr = tonemap.process(hdr)
+# Global tone mapping
+image_mapped = globalToneMapping(hdr_image, gamma=0.6)
 
-merge_mertens = cv2.createMergeMertens()
-fusion = merge_mertens.process(images)
+    # Adjust image intensity based on the middle image from image stack
+template = images[len(images)//2]
+image_tuned = intensityAdjustment(image_mapped, template)
 
-# Tonemap operators create floating point images with values in the 0..1 range
-# This is why we multiply the image with 255 before saving
-cv2.imwrite('durand_image.png', ldr * 255)
-cv2.imwrite('fusion.png', fusion * 255)
+    # Output image
+output = cv2.normalize(image_tuned, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+cv2.imwrite('durand_image.png', output)
+
